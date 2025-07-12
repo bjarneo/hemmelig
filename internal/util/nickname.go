3aab986e58ea3cd166364b89eca40672140c4f03
Bjarne <bjarne.oeverli@gmail.com>
2025-07-12 22:24:29 +0200

    feat: add more nick names

--- internal/util/nickname.go
+++ internal/util/nickname.go
@@ -13,6 +13,10 @@ func GenerateRandomNickname() string {
 		"Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu", "Red", "Blue", "Green", "Gold",
 		"Silver", "Bronze", "Ruby", "Sapphire", "Emerald", "Diamond", "Topaz", "Garnet", "Jade", "Opal",
 		"Agent", "Rogue", "Cipher", "Specter", "Ghost", "Shadow", "Phantom", "Wraith", "Viper", "Cobra",
+		"Fenrir", "Jormungandr", "Sleipnir", "Gungnir", "Mjolnir", "Ragnar", "Bjorn", "Floki", "Ivar",
+		"Sigurd", "Valkyrie", "Aslaug", "Skadi", "Hrafn", "Eirik", "ZeroCool", "AcidBurn", "Neo",
+		"Trinity", "Morpheus", "Cypher", "Proxy", "Payload", "Root", "Kernel", "Daemon", "Null",
+		"Byte", "Glitch", "Alias", "Hemmelig", "Secret",
 	}
 	name := names[rand.Intn(len(names))]
 	tag := rand.Intn(90000) + 10000 // Generate a 5-digit number
